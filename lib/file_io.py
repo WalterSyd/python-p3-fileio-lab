acc6eb72125f41008ea180b36b33730e89444166
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-09-07 17:51:07 +0300

    Final commit

--- lib/file_io.py
+++ lib/file_io.py
@@ -3,11 +3,22 @@ def write_file(file_name, file_content):
     file_name_str = str(file_name)
     #Add a .txt extension
     file_name_with_extension = file_name_str + '.txt'
+    #Write to open,file & close file
     with open(file_name_with_extension, 'w') as file:
         file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    file_name_str = str(file_name)
+    file_name_with_extension = file_name_str + '.txt'
+    with open(file_name_with_extension, 'a') as file:
+        #
+        file.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    file_name_str = str(file_name)
+    file_name_with_extension = file_name_str + '.txt'
+    with open(file_name_with_extension, "r") as fyle:
+        fyle.content = fyle.read()
+    return fyle.content
+
