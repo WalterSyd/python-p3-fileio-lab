f36227fdae335f404e5f0719dc7a11311890b68d
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-09-07 17:36:47 +0300

    Defined write file function

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,5 +1,10 @@
 def write_file(file_name, file_content):
-    pass
+    #convert file_name to string
+    file_name_str = str(file_name)
+    #Add a .txt extension
+    file_name_with_extension = file_name_str + '.txt'
+    with open(file_name_with_extension, 'w') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
     pass
